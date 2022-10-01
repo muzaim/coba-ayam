b29e2a3e39ea0df654ad1b6d0ea56424677cb7d3
muzaim <muzaim.surya@yahoo.co.id>
2022-10-01 13:30:23 +0700

    naik

--- src/Page/MainPage/index.js
+++ src/Page/MainPage/index.js
@@ -16,7 +16,7 @@ import {
 } from "../index";
 
 const MainPage = () => {
-  const [step, setStep] = useState("Menu");
+  const [step, setStep] = useState("Page6");
 
   const goToPage2 = () => {
     setStep("Page2");
