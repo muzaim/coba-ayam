94c2a4170416fae7c3c022c0ebb849ca5f2f568c
muzaim <muzaim.surya@yahoo.co.id>
2022-10-01 13:42:58 +0700

    a

--- src/Page/MainPage/index.js
+++ src/Page/MainPage/index.js
@@ -16,7 +16,7 @@ import {
 } from "../index";
 
 const MainPage = () => {
-  const [step, setStep] = useState("Main");
+  const [step, setStep] = useState("Menu");
 
   const goToPage2 = () => {
     setStep("Page2");
