c7272d9813ac343782d23b3af3d28200cdfdbe1d
muzaim <muzaim.surya@yahoo.co.id>
2022-11-02 10:25:32 +0700

    dsad

--- src/Page/Market/Page13/index.js
+++ src/Page/Market/Page13/index.js
@@ -203,9 +203,76 @@ const Page13 = ({
         </div>
         {/* HEADER END */}
         {/* CONTENT */}
-        <div className="w-full h-full bg-white">
-          <div className="flex justify-start w-full h-full">
-            <img src={Jualan} alt="" className=" bg-green-300 mb-12" />
+        <div className="w-full h-full">
+          <div className="relative justify-start w-full h-full">
+            {/* ORANG NPC */}
+            <div className="absolute w-full h-32  bottom-[7rem] z-10">
+              <div className="absolute">
+                <img src={avaArray[indexAvatar + 5]} alt="" className="w-24 " />
+              </div>
+              <div className="absolute left-20">
+                <img src={avaArray[indexAvatar + 4]} alt="" className="w-24 " />
+              </div>
+              <div className="absolute left-40">
+                <img src={avaArray[indexAvatar + 3]} alt="" className="w-24 " />
+              </div>
+              <div className="absolute left-60">
+                <img src={avaArray[indexAvatar + 2]} alt="" className="w-24 " />
+              </div>
+              <div className="absolute left-[25rem]">
+                <img src={avaArray[indexAvatar]} alt="" className="w-24 " />
+              </div>
+            </div>
+            {/* STAND JUALAN */}
+            <div className="absolute z-20">
+              <img src={Jualan} alt="" className="" />
+            </div>
+            {/* BUTTON JUAL SKIP */}
+            <div className="absolute right-3 z-20 top-[6.5rem]">
+              <div className="w-48 h-32 my-auto">
+                <div className="w-full h-full flex   items-center align-middle">
+                  <div className="w-full flex flex-col gap-2  place-items-center">
+                    <div
+                      className="w-[90%] h-full py-2 bg-blue-500 rounded-full text-center"
+                      onClick={tanyaJualMarket}
+                    >
+                      <span className="capitalize text-xl text-white tracking-widest">
+                        jual
+                      </span>
+                    </div>
+                    <div
+                      className="w-[90%] h-full py-2 bg-green-500 rounded-full text-center"
+                      onClick={nextDialog}
+                    >
+                      <span className="capitalize text-xl text-white tracking-widest">
+                        skip
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* DIALOG NPC */}
+            <div className="absolute z-30 right-[14rem] top-2 animate-fadeInKu">
+              <div className="w-48 h-24 p-3 bg-[#782443] rounded-xl ml-5 ring-offset-2 ring-4 ring-[#782443] ">
+                <div className="w-full h-full text-center items-center flex mx-auto">
+                  <p className="text-white capitalize">{dialogArray[index]}</p>
+                </div>
+              </div>
+            </div>
+            {/* TUTUP TOKO */}
+            <div className="absolute z-30  left-16 bottom-20 ">
+              <div
+                className="w-40 h-10 bg-[#329bd1] rounded-full items-center flex active:bg-[#51a9d5]"
+                onClick={openBackDialog}
+              >
+                <div className="w-full text-center ">
+                  <span className="font-bold  text-sm text-white tracking-widest">
+                    Tutup Toko
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         {/* CONTENT END*/}
