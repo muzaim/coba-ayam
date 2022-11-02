351e5ba11b62a922c2ef06989848ca78037e7242
muzaim <muzaim.surya@yahoo.co.id>
2022-11-02 10:33:28 +0700

    naik

--- src/Page/Market/Page13/index.js
+++ src/Page/Market/Page13/index.js
@@ -187,7 +187,7 @@ const Page13 = ({
   }, []);
 
   return (
-    <div className="w-full h-screen overflow-hidden bg-caffe bg-cover mx-auto lg:max-w-6xl lg:h-[70%]">
+    <div className="w-full h-full overflow-hidden bg-caffe bg-cover mx-auto lg:max-w-6xl lg:h-[70%]">
       <div className="w-full h-full mx-auto">
         {/* HEADER */}
         <div className="w-[90%] h-[15%] mx-auto">
@@ -203,7 +203,7 @@ const Page13 = ({
         </div>
         {/* HEADER END */}
         {/* CONTENT */}
-        <div className="w-full h-full">
+        <div className="w-full h-[85%] bg-white">
           <div className="relative justify-start w-full h-full">
             {/* ORANG NPC */}
             <div className="absolute w-full h-32  bottom-[7rem] z-10">
